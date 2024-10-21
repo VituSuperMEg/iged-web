f509e57e7af2cf85425b7719e22b4d432aa8075a
VituSuperMEg <vitorpires8555@gmail.com>
2024-10-21 08:00:49 -0300

    mudanças no env

--- src/services/api.ts
+++ src/services/api.ts
@@ -12,8 +12,8 @@ const getToken = () => {
 export type ApiMethods = "get" | "post" | "put" | "delete" | "patch";
 
 export const api = axios.create({
-  // baseURL: "https://api-digitalizacao-nest.vercel.app/",
-  baseURL: "http://localhost:3333",
+  baseURL: "https://api-digitalizacao-nest.vercel.app/",
+  // baseURL: "http://localhost:3333",
   headers: {
     "x-cliente-id": "0",
   },
